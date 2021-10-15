18ee882a62feedb03326fcfdb89926661d641ac3
Leo <bendezujonathan20@gmail.com>
2021-10-15 16:44:07 -0300

    Init

---
+++ src/main/java/TSBHashTableDA.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+import java.util.AbstractMap;
+import java.util.Map;
+
+public class TSBHashTableDA<K,V> extends AbstractMap implements Map<K,V>, Cloneable, Serializable {
+}
