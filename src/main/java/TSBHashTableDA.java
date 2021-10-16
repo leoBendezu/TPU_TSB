383016778764fbfa398a10a850d9a1ffcf079bef
Leo <bendezujonathan20@gmail.com>
2021-10-16 09:30:17 -0300

    Update TSBHashTableDA.java

--- src/main/java/TSBHashTableDA.java
+++ src/main/java/TSBHashTableDA.java
@@ -3,4 +3,10 @@ import java.util.AbstractMap;
 import java.util.Map;
 
 public class TSBHashTableDA<K,V> extends AbstractMap implements Map<K,V>, Cloneable, Serializable {
+
+    private Map.Entry<K,V> table[];
+    private int count;
+
+    private class Entry<K,V> implements Map.Entry< K, V> {
+    }
 }
