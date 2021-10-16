e0b0478f8aec478be5edb26f792f26d9fc28f03f
Leo <bendezujonathan20@gmail.com>
2021-10-16 10:32:21 -0300

    Update TSBHashTableDA.java

--- src/main/java/TSBHashTableDA.java
+++ src/main/java/TSBHashTableDA.java
@@ -1,12 +1,39 @@
 import java.io.Serializable;
-import java.util.AbstractMap;
-import java.util.Map;
+import java.util.*;
 
 public class TSBHashTableDA<K,V> extends AbstractMap implements Map<K,V>, Cloneable, Serializable {
 
     private Map.Entry<K,V> table[];
+    private int initialCapacity;
     private int count;
+    private float loadFactor;
+
+    private transient Set<K> keySet = null;
+    private transient Set<Map.Entry<K,V>> entrySet = null;
+    private transient Collection<V> values = null;
+
+    private transient  int modCount;
+
 
     private class Entry<K,V> implements Map.Entry< K, V> {
+        private K key;
+        private V value;
+        private int condition;
+
+        public Entry(K key, V value) {
+
+        }
+    }
+
+    private class KeySet extends AbstractSet<K> {
+
+    }
+
+    private class EntrySet extends AbstractSet<Map.Entry<K,V>> {
+
+    }
+
+    private class ValueColeection extends  AbstractCollection<V> {
+        
     }
 }
